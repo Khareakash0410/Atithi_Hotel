d68eda2b443dce890e6c7a880318d007a5a2f245
Akash Khare <akashkhare706882@gmail.com>
2025-05-11 14:08:05 +0530

    update nect config file

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com", "images.unsplash.com", "plus.unsplash.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
